668534ddf6fd59036e2c8b5d0b0f810f8f9686bb
Junv Zhao <wahyd4@gmail.com>
2017-08-31 09:21:07 +0800

    Add ability to enable rpc SSL.

--- caddy/conf/aria2c.sh
+++ caddy/conf/aria2c.sh
@@ -4,7 +4,7 @@ echo "Run aria2c and ariaNG"
 if [ "$SSL" = "true" ]; then
 echo "Start aria2 with secure config"
 
-/usr/bin/aria2c --conf-path="/root/conf/aria2.conf" -D --enable-rpc --rpc-listen-all --rpc-certificate=/root/conf/key/aria2.crt --rpc-private-key=/root/conf/key/aria2.key --rpc-secret=HelloJunv --rpc-secure \
+/usr/bin/aria2c --conf-path="/root/conf/aria2.conf" -D --enable-rpc --rpc-listen-all --rpc-certificate=/root/conf/key/aria2.crt --rpc-private-key=/root/conf/key/aria2.key --rpc-secret="$RPC_SECRET" --rpc-secure \
 && caddy -quic --conf /usr/local/caddy/Caddyfile
 
 else
