f96ad5c97f48d9639b0fb89a0c8e9888a0c7f1ef
Junv Zhao <wahyd4@gmail.com>
2017-08-31 17:45:27 +0800

    Add ability to enable basic auth.

--- caddy/conf/aria2c.sh
+++ caddy/conf/aria2c.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 echo "Run aria2c and ariaNG"
+echo $ENABLE_AUTH
+if [ "$ENABLE_AUTH" = "true" ]; then
+  echo "using basic auth config file "
+  CADDY_FILE=/usr/local/caddy/SecureCaddyfile
+else
+  CADDY_FILE=/usr/local/caddy/Caddyfile
+fi
 
 if [ "$SSL" = "true" ]; then
 echo "Start aria2 with secure config"
 
 /usr/bin/aria2c --conf-path="/root/conf/aria2.conf" -D --enable-rpc --rpc-listen-all --rpc-certificate=/root/conf/key/aria2.crt --rpc-private-key=/root/conf/key/aria2.key --rpc-secret="$RPC_SECRET" --rpc-secure \
-&& caddy -quic --conf /usr/local/caddy/Caddyfile
+&& caddy -quic --conf ${CADDY_FILE}
 
 else
 echo "Start aria2 with standard mode"
 /usr/bin/aria2c --conf-path="/root/conf/aria2.conf" -D --enable-rpc --rpc-listen-all \
-&& caddy -quic --conf /usr/local/caddy/Caddyfile
+&& caddy -quic --conf ${CADDY_FILE}
 
 fi
 
