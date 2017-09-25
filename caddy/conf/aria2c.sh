2f40100ed255f2a744e5baf11fc302133fce490b
Junv Zhao <wahyd4@gmail.com>
2017-09-25 13:07:53 +0800

    Update docker file by enable caddy filemanager plugin.

--- caddy/conf/aria2c.sh
+++ caddy/conf/aria2c.sh
@@ -11,14 +11,18 @@ fi
 if [ "$SSL" = "true" ]; then
 echo "Start aria2 with secure config"
 
-/usr/bin/aria2c --conf-path="/root/conf/aria2.conf" -D --enable-rpc --rpc-listen-all --rpc-certificate=/root/conf/key/aria2.crt --rpc-private-key=/root/conf/key/aria2.key --rpc-secret="$RPC_SECRET" --rpc-secure \
+/usr/bin/aria2c --conf-path="/root/conf/aria2.conf" -D  \
+--enable-rpc --rpc-listen-all  \
+--rpc-certificate=/root/conf/key/aria2.crt \
+--rpc-private-key=/root/conf/key/aria2.key \
+--rpc-secret="$RPC_SECRET" --rpc-secure \
 && caddy -quic --conf ${CADDY_FILE}
 
 else
 echo "Start aria2 with standard mode"
-/usr/bin/aria2c --conf-path="/root/conf/aria2.conf" -D --enable-rpc --rpc-listen-all \
+/usr/bin/aria2c --conf-path="/root/conf/aria2.conf" -D \
+--enable-rpc --rpc-listen-all \
 && caddy -quic --conf ${CADDY_FILE}
-
 fi
 
 
