5f3d232f3a7d74bd7c3d12bef12698b18823b8e4
Junv Zhao <wahyd4@gmail.com>
2019-07-13 13:16:16 +1000

    Using forogo to manage mutliple processes

--- aria2c.sh
+++ aria2c.sh
@@ -1,30 +1,17 @@
 #! /bin/bash -eu
 
-echo "Run aria2c and ariaNG"
-if [ "$ENABLE_AUTH" = "true" ]; then
-  echo "Using Basic Auth config file "
-  CADDY_FILE=/usr/local/caddy/SecureCaddyfile
-else
-  echo "Using caddy without Basic Auth"
-  CADDY_FILE=/usr/local/caddy/Caddyfile
-fi
-
 if [ "$SSL" = "true" ]; then
 echo "Start aria2 with secure config"
 
-/usr/bin/aria2c --conf-path="/root/conf/aria2.conf" -D  \
---enable-rpc --rpc-listen-all  \
---rpc-certificate=/root/conf/key/aria2.crt \
---rpc-private-key=/root/conf/key/aria2.key \
+/usr/bin/aria2c --conf-path="/app/conf/aria2.conf" \
+--enable-rpc --rpc-listen-all \
+--rpc-certificate=/app/conf/key/aria2.crt \
+--rpc-private-key=/app/conf/key/aria2.key \
 --rpc-secret="$RPC_SECRET" --rpc-secure \
-&& (nohup sh -c "/usr/local/bin/filebrowser -p 8080 -d /root/filebrowser.db -l /var/log/file-browser/out.log -r /data" &)\
-&& /usr/local/bin/caddy -quic --conf ${CADDY_FILE}
 
 else
 
 echo "Start aria2 with standard mode"
-/usr/bin/aria2c --conf-path="/root/conf/aria2.conf" -D \
---enable-rpc --rpc-listen-all \
-&& (nohup sh -c "/usr/local/bin/filebrowser -p 8080 -d /root/filebrowser.db -l /var/log/file-browser/out.log -r /data" &)\
-&& /usr/local/bin/caddy -quic --conf ${CADDY_FILE}
+/usr/bin/aria2c --conf-path="/app/conf/aria2.conf" --enable-rpc --rpc-listen-all
+
 fi
