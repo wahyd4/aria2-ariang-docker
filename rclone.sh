c0e66890adeb99c7d5b68627c25948f5206a7ed5
Junwei Zhao <wahyd4@gmail.com>
2020-09-13 00:06:01 +1000

    Update webgui cache folder to solve the permission issue for non root user

--- rclone.sh
+++ rclone.sh
@@ -5,4 +5,5 @@ rclone rcd --rc-web-gui \
   --rc-web-gui-no-open-browser \
   --rc-addr :5572 \
   --rc-user $ARIA2_USER \
-  --rc-pass $ARIA2_PWD
+  --rc-pass $ARIA2_PWD \
+  --cache-dir /app/.cache
