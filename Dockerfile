9e4a2b6b9d155bf23b402c3b70132a3c5fcddd8c
Junwei Zhao <wahyd4@gmail.com>
2021-01-01 17:05:38 +1100

    Using multi arch to build docker image for multiple archtectures

--- Dockerfile
+++ Dockerfile
@@ -18,46 +18,11 @@ ENV RCLONE_CONFIG=/app/conf/rclone.conf
 ENV XDG_DATA_HOME=/app/.caddy/data
 ENV XDG_CONFIG_HOME=/app/.caddy/config
 
-ADD aria2c.sh caddy.sh Procfile init.sh start.sh rclone.sh /app/
+ADD install.sh aria2c.sh caddy.sh Procfile init.sh start.sh rclone.sh /app/
 ADD conf /app/conf
 ADD Caddyfile SecureCaddyfile /usr/local/caddy/
 
-RUN adduser -D -u 1000 junv \
-  && apk update \
-  && apk add runit shadow wget bash curl openrc gnupg aria2 tar mailcap --no-cache \
-  && caddy_tag=2.2.1 \
-  && wget -N https://github.com/caddyserver/caddy/releases/download/v${caddy_tag}/caddy_${caddy_tag}_linux_amd64.tar.gz \
-  && tar -zxvf caddy_${caddy_tag}_linux_amd64.tar.gz \
-  && mv caddy /usr/local/bin/ \
-  && rm -rf caddy_${caddy_tag}_linux_amd64.tar.gz \
-  && filebrowser_version=v2.11.0 \
-  && platform=linux-amd64 \
-  && wget -N https://github.com/filebrowser/filebrowser/releases/download/${filebrowser_version}/${platform}-filebrowser.tar.gz \
-  && tar -zxvf ${platform}-filebrowser.tar.gz \
-  && rm -rf ${platform}-filebrowser.tar.gz \
-  && rm LICENSE README.md CHANGELOG.md \
-  && wget -N https://bin.equinox.io/c/ekMN3bCZFUn/forego-stable-${platform}.tgz \
-  && tar -zxvf forego-stable-${platform}.tgz \
-  && rm -rf forego-stable-${platform}.tgz \
-  && mkdir -p /usr/local/www \
-  && mkdir -p /usr/local/www/aria2 \
-  && rm -rf init /app/*.txt \
-  && curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip \
-  && unzip rclone-*.zip \
-  && cd rclone-*-linux-amd64 \
-  && cp rclone /usr/local/bin/ \
-  && chown junv:junv /usr/local/bin/rclone \
-  && chmod 755 /usr/local/bin/rclone \
-  && rm /app/rclone-*.zip \
-  && rm -rf /app/rclone-* \
-  && mkdir /usr/local/www/aria2/Download \
-  && cd /usr/local/www/aria2 \
-  && chmod +rw /app/conf/aria2.session \
-  && version=1.1.7 \
-  && wget -N --no-check-certificate https://github.com/mayswind/AriaNg/releases/download/${version}/AriaNg-${version}.zip \
-  && unzip AriaNg-${version}.zip \
-  && rm -rf AriaNg-${version}.zip \
-  && chmod -R 755 /usr/local/www/aria2
+RUN ./install.sh
 
 # folder for storing ssl keys
 VOLUME /app/conf/key
---
+++ install.sh
@@ -0,0 +1,73 @@
+#! /bin/sh -eux
+
+echo "Set variables for $(arch)"
+
+caddy_version=2.3.0
+filebrowser_version=v2.11.0
+rclone_version=v1.53.3
+ariang_version=1.1.7
+
+case "$(arch)" in
+
+   x86_64)
+      platform=linux-amd64
+      caddy_file=caddy_${caddy_version}_linux_amd64.tar.gz
+      rclone_file=rclone-${rclone_version}-${platform}.zip
+      forego_file=forego-stable-${platform}.tgz
+     ;;
+   armv7l)
+     platform=linux-armv7
+     caddy_file=caddy_${caddy_version}_linux_armv7.tar.gz
+     rclone_file=rclone-${rclone_version}-linux-arm-v7.zip
+     forego_file=forego-stable-linux-arm.tgz
+     ;;
+
+   aarch64)
+     platform=linux-arm64
+     caddy_file=caddy_${caddy_version}_linux_arm64.tar.gz
+     rclone_file=rclone-${rclone_version}-${platform}.zip
+     forego_file=forego-stable-${platform}.tgz
+     ;;
+
+   *)
+     echo "unsupported arch $(arch), exit now"
+     exit 1
+     ;;
+esac
+
+filebrowser_file=${platform}-filebrowser.tar.gz
+ariang_file=AriaNg-${ariang_version}.zip
+
+adduser -D -u 1000 junv \
+  && apk update \
+  && apk add runit shadow wget bash curl openrc gnupg aria2 tar mailcap --no-cache \
+  && wget -N https://github.com/caddyserver/caddy/releases/download/v${caddy_version}/${caddy_file} \
+  && tar -zxvf ${caddy_file} \
+  && mv caddy /usr/local/bin/ \
+  && rm -rf ${caddy_file} \
+  && platform=linux-amd64 \
+  && wget -N https://github.com/filebrowser/filebrowser/releases/download/${filebrowser_version}/${filebrowser_file} \
+  && tar -zxvf ${filebrowser_file} \
+  && rm -rf ${filebrowser_file} \
+  && rm LICENSE README.md CHANGELOG.md \
+  && wget -N https://bin.equinox.io/c/ekMN3bCZFUn/${forego_file} \
+  && tar -zxvf ${forego_file} \
+  && rm -rf ${forego_file} \
+  && mkdir -p /usr/local/www \
+  && mkdir -p /usr/local/www/aria2 \
+  && rm -rf init /app/*.txt \
+  && curl -O https://downloads.rclone.org/${rclone_version}/${rclone_file} \
+  && unzip ${rclone_file} \
+  && cd rclone-* \
+  && cp rclone /usr/local/bin/ \
+  && chown junv:junv /usr/local/bin/rclone \
+  && chmod 755 /usr/local/bin/rclone \
+  && rm /app/${rclone_file} \
+  && rm -rf /app/rclone-* \
+  && mkdir /usr/local/www/aria2/Download \
+  && cd /usr/local/www/aria2 \
+  && chmod +rw /app/conf/aria2.session \
+  && wget -N --no-check-certificate https://github.com/mayswind/AriaNg/releases/download/${ariang_version}/${ariang_file} \
+  && unzip ${ariang_file} \
+  && rm -rf ${ariang_file} \
+  && chmod -R 755 /usr/local/www/aria2
