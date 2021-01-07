90a4e37726da1af429c0153d834b54de0e514d32
Junwei Zhao <wahyd4@gmail.com>
2021-01-07 16:20:10 +1100

    offer a way to disable rclone

--- rclone.sh
+++ rclone.sh
@@ -1,9 +1,14 @@
 #! /bin/bash -eu
 
-echo "Start Rclone"
-rclone rcd --rc-web-gui \
-  --rc-web-gui-no-open-browser \
-  --rc-addr :5572 \
-  --rc-user $ARIA2_USER \
-  --rc-pass $ARIA2_PWD \
-  --cache-dir /app/.cache
+if [ "$ENABLE_RCLONE" = "true" ]; then
+  echo "Start Rclone, please make sure you can connect to Github website. if not, please set docker env ENABLE_RCLONE=false"
+  rclone rcd --rc-web-gui \
+    --rc-web-gui-no-open-browser \
+    --rc-addr :5572 \
+    --rc-user $ARIA2_USER \
+    --rc-pass $ARIA2_PWD \
+    --cache-dir /app/.cache
+else
+  echo "Skip starting Rclone as it's been disabled"
+  sleep 3650d
+fi
