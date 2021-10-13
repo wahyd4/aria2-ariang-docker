2c5396567b2dcccdc120cd2f7f54d9f1f2ff0cfd
Junwei Zhao <wahyd4@gmail.com>
2021-10-13 22:46:44 +1100

    Add logic to allow user check new version

--- Procfile
+++ Procfile
@@ -2,3 +2,4 @@ caddy: /app/caddy.sh
 filebrowser: /app/filebrowser -p 8080 -d /app/filebrowser.db -r /data -b /files
 aria2c: /app/aria2c.sh
 rclone: /app/rclone.sh
+newer_version_checker: /app/new-version-checker.sh
---
+++ new-version-checker.sh
@@ -0,0 +1,24 @@
+#! /bin/bash -u
+sleep 5
+
+set -e
+APP_VERSION=$(cat APP_VERSION)
+set +e
+while true; do
+  echo "[INFO] $(date -u +'%Y-%m-%dT%H:%M:%SZ') Checking for new version against ${APP_VERSION} ..."
+  newer_version=$(curl -s https://badges.toozhao.com/val/aria2-ui-docker?version=${APP_VERSION})
+  if [ -z ${newer_version} ]; then
+    echo "[WARN] $(date -u +'%Y-%m-%dT%H:%M:%SZ') Failed to check new version."
+    sleep 86400
+    continue
+  fi
+
+  if [ ${newer_version} != ${APP_VERSION} ]; then
+    echo "[INFO] $(date -u +'%Y-%m-%dT%H:%M:%SZ') Found newer Docker image version wahyd4/aria2-ui:${newer_version}, please consider to upgrade by using docker pull command to have better user experience."
+    # check new version daily
+    sleep 86400
+    continue
+  fi
+  echo "[INFO] $(date -u +'%Y-%m-%dT%H:%M:%SZ') Good job, you are using the latest version docker image"
+  sleep 86400
+done
