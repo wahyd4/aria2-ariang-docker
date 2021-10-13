f7e9827ffd77d9a74bd460aeaa6f6f41085a38b1
Junwei Zhao <wahyd4@gmail.com>
2021-10-13 23:48:10 +1100

    Refine the logic of app new version checker

--- new-version-checker.sh
+++ new-version-checker.sh
@@ -6,14 +6,14 @@ APP_VERSION=$(cat APP_VERSION)
 set +e
 while true; do
   echo "[INFO] $(date -u +'%Y-%m-%dT%H:%M:%SZ') Checking for new version against ${APP_VERSION} ..."
-  newer_version=$(curl -s https://badges.toozhao.com/val/aria2-ui-docker?version=${APP_VERSION})
-  if [ -z ${newer_version} ]; then
+  newer_version=$(curl --max-time 6 -s https://badges.toozhao.com/val/aria2-ui-docker?version=${APP_VERSION})
+  if [[ -z ${newer_version} ]]; then
     echo "[WARN] $(date -u +'%Y-%m-%dT%H:%M:%SZ') Failed to check new version."
     sleep 86400
     continue
   fi
 
-  if [ ${newer_version} != ${APP_VERSION} ]; then
+  if [[ ${newer_version} > ${APP_VERSION} ]]; then
     echo "[INFO] $(date -u +'%Y-%m-%dT%H:%M:%SZ') Found newer Docker image version wahyd4/aria2-ui:${newer_version}, please consider to upgrade by using docker pull command to have better user experience."
     # check new version daily
     sleep 86400
