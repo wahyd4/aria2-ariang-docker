6aefdf848e5a84f26b80f1a832587d69e7a2d143
Junwei Zhao <wahyd4@gmail.com>
2023-05-21 19:41:17 +1000

    Switch to debian, which might fix some weired issues in RasberryPi

--- install.sh
+++ install.sh
@@ -34,10 +34,10 @@ esac
 
 filebrowser_file=${platform}-filebrowser.tar.gz
 ariang_file=AriaNg-${ariang_version}.zip
-
-adduser -D -u 1000 junv \
-  && apk update \
-  && apk add runit shadow wget bash curl openrc gnupg aria2 tar mailcap fuse vim libcap --no-cache \
+groupadd junv \
+  && useradd -g junv -m -s /bin/bash junv \
+  && apt update \
+  && apt install -y runit wget curl openrc gnupg aria2 tar mailcap fuse vim unzip libcap2-bin\
   && wget -N https://github.com/caddyserver/caddy/releases/download/v${caddy_version}/${caddy_file} \
   && tar -zxf ${caddy_file} \
   && mv caddy /usr/local/bin/ \
@@ -65,6 +65,7 @@ adduser -D -u 1000 junv \
   && unzip ${ariang_file} \
   && rm -rf ${ariang_file} \
   && chmod -R 755 /usr/local/www/aria2 \
+  && mkdir -p /data && chown junv:junv /data \
   && mkdir -p /data/cloud \
   && chown junv:junv /data/cloud \
   && ln -s /data/cloud /app
