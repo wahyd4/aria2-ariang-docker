6c3caac6874788c133369603706a54d3a0b45436
divyam234 <47589864+divyam234@users.noreply.github.com>
2023-09-18 04:22:04 +0530

    fix sed  escaping (#239)

--- caddy.sh
+++ caddy.sh
@@ -9,15 +9,15 @@ case $ENABLE_AUTH in
 true)
   echo "[INFO] Use Basic Auth config file "
   export CADDY_FILE=/usr/local/caddy/SecureCaddyfile
-  sed -i 's/ARIA2_USER/'"${ARIA2_USER}"'/g' ${CADDY_FILE}
-  sed -i 's/ARIA2_PWD_ENCRYPT/'"${ARIA2_PWD_ENCRYPT}"'/g' ${CADDY_FILE}
+  sed -i "s#ARIA2_USER#${ARIA2_USER}#g" ${CADDY_FILE}
+  sed -i "s#ARIA2_PWD_ENCRYPT#${ARIA2_PWD_ENCRYPT}#g" ${CADDY_FILE}
   ;;
 
 heroku)
   echo "[INFO] Run Caddy with Heroku mode"
   export CADDY_FILE=/usr/local/caddy/HerokuCaddyfile
-  sed -i 's/ARIA2_USER/'"${ARIA2_USER}"'/g' ${CADDY_FILE}
-  sed -i 's/ARIA2_PWD_ENCRYPT/'"${ARIA2_PWD_ENCRYPT}"'/g' ${CADDY_FILE}
+  sed -i "s#ARIA2_USER#${ARIA2_USER}#g" ${CADDY_FILE}
+  sed -i "s#ARIA2_PWD_ENCRYPT#${ARIA2_PWD_ENCRYPT}#g" ${CADDY_FILE}
 
   sed -i 's/PORT/'"${PORT}"'/g' ${CADDY_FILE}
 
