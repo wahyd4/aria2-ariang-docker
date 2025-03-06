ea0dbb9e1475c833000f9bcd54a6ae2ea2d4212e
Junwei Zhao <wahyd4@gmail.com>
2025-03-06 22:49:55 +1100

    Add logic to allow user disable filebrowser (#245)

---
+++ filebrowser.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eu
+
+echo "Checking if FileBrowser is enabled..."
+
+if [ "$ENABLE_FILEBROWSER" = "true" ]; then
+    echo "Starting FileBrowser..."
+    /app/filebrowser -p 8080 -d /app/filebrowser.db -r /data -b /files
+else
+    echo "FileBrowser is disabled. Set ENABLE_FILEBROWSER=true to enable it."
+fi
