9fb612352016d612c900c02d5f83a0932936f2f6
Junwei Zhao <wahyd4@gmail.com>
2025-03-06 23:10:19 +1100

    Urgent bug fix

--- filebrowser.sh
+++ filebrowser.sh
@@ -7,4 +7,5 @@ if [ "$ENABLE_FILEBROWSER" = "true" ]; then
     /app/filebrowser -p 8080 -d /app/filebrowser.db -r /data -b /files
 else
     echo "FileBrowser is disabled. Set ENABLE_FILEBROWSER=true to enable it."
+    sleep 3650d
 fi
